584c9701ce0774fef8cf0bc3120d4a57b32e16d2
bleleet <bleleet@gmail.com>
2020-11-25 10:46:25 +0900

    modify function keys

--- keymaps/bleleet/keymap.c
+++ keymaps/bleleet/keymap.c
@@ -1,6 +1,6 @@
 /* Copyright 2020 Bleleet <bleleet@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
+ *  This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
@@ -21,7 +21,8 @@ enum atreus_layers {
     _QWERTY,
     _LOWER,
     _RAISE,
-    _ADJUST
+    _ADJUST,
+    _GAME
 };
 
 enum atreus_keycodes {
@@ -37,36 +38,36 @@ enum atreus_keycodes {
 
 #define LOWER LT(_LOWER,KC_SPC)
 #define RAISE LT(_RAISE,KC_BSPC)
-#define _SECH LCMD(KC_SPC)
-#define _HOME LCMD(KC_H)
-#define _TMUX LCTL(KC_B)
-#define _MTRSFT RSFT_T(KC_ENT)
-#define _MTLSFT LSFT_T(KC_Z)
-#define _OSMCMD OSM(MOD_LGUI)
-#define _OSMCTL OSM(MOD_LCTL)
-#define _OSMALT OSM(MOD_LALT)
-#define _OSMSFT OSM(MOD_LSFT)
+#define GAME DF(_GAME)
+#define SPOT LCMD(KC_SPC)
+#define TMUX LCTL(KC_B)
+#define MTRSFT RSFT_T(KC_ENT)
+#define MTLSFT LSFT_T(KC_Z)
+#define OSMCMD OSM(MOD_LGUI)
+#define OSMCTL OSM(MOD_LCTL)
+#define OSMALT OSM(MOD_LALT)
+#define OSMSFT OSM(MOD_LSFT)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_ESC,
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-    _MTLSFT, KC_X,    KC_C,    KC_D,    KC_V,    _HOME,   _SECH,   KC_K,    KC_H,    KC_COMM, KC_DOT,  _MTRSFT,
-    _OSMCMD, _OSMCTL, _OSMALT,  KC_TAB, LOWER,   _OSMCTL, _OSMCMD, RAISE,   _TMUX,  _OSMALT, _OSMCMD, _OSMCTL
+    MTLSFT,  KC_X,    KC_C,    KC_D,    KC_V,    _______, GAME,    KC_K,    KC_H,    KC_COMM, KC_DOT,  MTRSFT,
+    OSMCTL,  OSMALT,  SPOT,    KC_TAB,  LOWER,   OSMCMD,  OSMCMD,  RAISE,   KC_ESC,  TMUX,    OSMALT,  OSMCTL
   ),
 
   [_QWERTY] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,
-    _MTLSFT, KC_X,    KC_C,    KC_V,    KC_B,    _HOME,   _SECH,   KC_N,    KC_M,    KC_COMM, KC_DOT,  _MTRSFT,
-    _OSMCMD, _OSMCTL, _OSMALT,  KC_TAB, LOWER,   _OSMCTL, _OSMCMD, RAISE,   _TMUX,  _OSMALT, _OSMCMD, _OSMCTL
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    MTLSFT,  KC_X,    KC_C,    KC_V,    KC_B,    _______, GAME,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MTRSFT,
+    OSMCTL,  OSMALT,  SPOT,    KC_TAB,  LOWER,   OSMCMD,  OSMCMD,  RAISE,   KC_ESC,  TMUX,    OSMALT,  OSMCTL
   ),
 
  [_LOWER] = LAYOUT(
     KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_MPLY,                   KC_RBRC, KC_RCBR, KC_QUES, KC_EXLM, KC_COLN,
     KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _LANG1,                    KC_RPRN, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,
-    _QUICK1, _QUICK2, _QUICK3, _QUICK4, _QUICK5, _______, _______, KC_DQUO, KC_QUOT, KC_SCLN, KC_LT,   KC_GT,
+    _QUICK1, _QUICK2, _QUICK3, _QUICK4, _QUICK5, _______, _______, KC_DQUO, KC_QUOT, KC_LT,   KC_GT,   _______,
     KC_MRWD, KC_VOLU, KC_VOLD, KC_MFFD, _______, _______, _______, _______, _______, _______, _______, _______
  ),
 
@@ -82,7 +83,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _LANG2,                    KC_F12,  KC_F4,   KC_F5,   KC_F6,   KC_F13,
     _______, _______, _______, _______, _______, _______, _______, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F11,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  KC_CAPS
- )
+ ),
+
+  [_GAME] = LAYOUT(
+    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, DF(_COLEMAK),_______,_______,_______,_______,_______,
+    _______, _______, _______, OSMALT,  OSMCTL,  OSMCMD,  _______, _______, _______, _______, _______, _______
+  )
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -95,7 +103,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch (keycode) {
             case _LANG1:
                 SEND_STRING(SS_LCTL(" "));
-                SEND_STRING(SS_TAP(X_ENT));
+                //SEND_STRING(SS_TAP(X_ENT));
             case _LANG2:
                 default_layer = biton32(default_layer_state);
                 if (default_layer == _COLEMAK) {

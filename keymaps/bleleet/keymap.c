c6025935fd3a052adfc4d141f55a98a4fa4af410
bleleet <bleleet@gmail.com>
2020-12-09 19:52:28 +0900

    ..

--- keymaps/bleleet/keymap.c
+++ keymaps/bleleet/keymap.c
@@ -21,28 +21,20 @@ enum atreus_layers {
     _QWERTY,
     _LOWER,
     _RAISE,
-    _ADJUST,
-    _GAME
+    _ADJUST
 };
 
 enum atreus_keycodes {
     _LANG1 = SAFE_RANGE,
     _LANG2,
-    _QUICK1,
-    _QUICK2,
-    _QUICK3,
-    _QUICK4,
-    _QUICK5,
     _HEXPAD
 };
 
 #define LOWER LT(_LOWER,KC_SPC)
 #define RAISE LT(_RAISE,KC_BSPC)
-#define GAME DF(_GAME)
 #define SPOT LCMD(KC_SPC)
 #define TMUX LCTL(KC_B)
-#define MTRSFT RSFT_T(KC_ENT)
-#define MTLSFT LSFT_T(KC_Z)
+#define HOTK LALT(KC_SPC)
 #define OSMCMD OSM(MOD_LGUI)
 #define OSMCTL OSM(MOD_LCTL)
 #define OSMALT OSM(MOD_LALT)
@@ -51,45 +43,38 @@ enum atreus_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_ESC,
     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-    MTLSFT,  KC_X,    KC_C,    KC_D,    KC_V,    _______, GAME,    KC_K,    KC_H,    KC_COMM, KC_DOT,  MTRSFT,
-    OSMCTL,  OSMALT,  SPOT,    KC_TAB,  LOWER,   OSMCMD,  OSMCMD,  RAISE,   KC_ESC,  TMUX,    OSMALT,  OSMCTL
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    HOTK,    HOTK,    KC_K,    KC_H,    KC_COMM, KC_SCLN, KC_ENT,
+    OSMCTL,  OSMALT,  SPOT,    KC_TAB,  LOWER,   OSMCMD,  OSMCMD,  RAISE,   OSMSFT,  TMUX,    OSMALT,  OSMCTL
   ),
 
   [_QWERTY] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    MTLSFT,  KC_X,    KC_C,    KC_V,    KC_B,    _______, GAME,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MTRSFT,
-    OSMCTL,  OSMALT,  SPOT,    KC_TAB,  LOWER,   OSMCMD,  OSMCMD,  RAISE,   KC_ESC,  TMUX,    OSMALT,  OSMCTL
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    HOTK,    HOTK,    KC_N,    KC_M,    KC_COMM, KC_SCLN, KC_ENT,
+    OSMCTL,  OSMALT,  SPOT,    KC_TAB,  LOWER,   OSMCMD,  OSMCMD,  RAISE,   OSMSFT,  TMUX,    OSMALT,  OSMCTL
   ),
 
- [_LOWER] = LAYOUT(
-    KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_MPLY,                   KC_RBRC, KC_RCBR, KC_QUES, KC_EXLM, KC_COLN,
-    KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _LANG1,                    KC_RPRN, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,
-    _QUICK1, _QUICK2, _QUICK3, _QUICK4, _QUICK5, _______, _______, KC_DQUO, KC_QUOT, KC_LT,   KC_GT,   _______,
-    KC_MRWD, KC_VOLU, KC_VOLD, KC_MFFD, _______, _______, _______, _______, _______, _______, _______, _______
- ),
+  [_LOWER] = LAYOUT(
+    KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _LANG2,                    S(KC_E), KC_7,    KC_8,    KC_9,    S(KC_F),
+    KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _LANG1,                    S(KC_C), KC_4,    KC_5,    KC_6,    S(KC_D),
+    KC_MRWD, KC_VOLU, KC_VOLD, KC_MFFD, KC_MPLY, _______, _______, S(KC_A), KC_1,    KC_2,    KC_3,    S(KC_B),
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    _HEXPAD, KC_DOT,  _______
+  ),
 
- [_RAISE] = LAYOUT(
-    KC_GRV,  KC_HASH, KC_PERC, KC_LCBR, KC_LBRC,                   S(KC_E), KC_7,    KC_8,    KC_9,    S(KC_F),
-    KC_CIRC, KC_PIPE, KC_AMPR, KC_ASTR, KC_LPRN,                   S(KC_C), KC_4,    KC_5,    KC_6,    S(KC_D),
-    KC_AT,   KC_DLR,  KC_TILD, KC_SLSH, KC_BSLS, _______, _______, S(KC_A), KC_1,    KC_2,    KC_3,    S(KC_B),
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    _HEXPAD, _______, _______
- ),
+  [_RAISE] = LAYOUT(
+	KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+	KC_QUOT, KC_AMPR, KC_ASTR, KC_SLSH, _______,                   _______, KC_PLUS, KC_MINS, KC_UNDS, KC_EQL,
+	KC_DLR,  KC_EXLM, KC_LPRN, KC_RPRN, _______, _______, _______, _______, KC_LT,   KC_GT,   KC_DOT,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
 
- [_ADJUST] = LAYOUT(
-    _______, _______, _______, _______, _______,                   KC_F14,  KC_F7,   KC_F8,   KC_F9,   KC_F15,
-    _______, _______, _______, _______, _LANG2,                    KC_F12,  KC_F4,   KC_F5,   KC_F6,   KC_F13,
-    _______, _______, _______, _______, _______, _______, _______, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F11,
+  [_ADJUST] = LAYOUT(
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_DQUO, KC_PIPE, KC_AT,   KC_BSLS, KC_F11,                    KC_F12,  KC_HASH, KC_PERC, KC_TILD, KC_GRV,
+    KC_CIRC, KC_QUES, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_COLN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  KC_CAPS
- ),
-
-  [_GAME] = LAYOUT(
-    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, DF(_COLEMAK),_______,_______,_______,_______,_______,
-    _______, _______, _______, OSMALT,  OSMCTL,  OSMCMD,  _______, _______, _______, _______, _______, _______
   )
 };
 
@@ -112,27 +97,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 else if (default_layer == _QWERTY) {
                     set_single_persistent_default_layer(_COLEMAK);
                 }
-                break;
-            case _QUICK1:
-                SEND_STRING("''");
-                SEND_STRING(SS_TAP(X_LEFT));
-                break;
-            case _QUICK2:
-                SEND_STRING("[]");
-                SEND_STRING(SS_TAP(X_LEFT));
-                break;
-            case _QUICK3:
-                SEND_STRING("()");
-                SEND_STRING(SS_TAP(X_LEFT));
-                break;
-            case _QUICK4:
-                SEND_STRING("\"\"");
-                SEND_STRING(SS_TAP(X_LEFT));
-                break;
-            case _QUICK5:
-                SEND_STRING("{}");
-                SEND_STRING(SS_TAP(X_LEFT));
-                SEND_STRING(SS_TAP(X_ENT));
                 break;
             case _HEXPAD:
                 SEND_STRING("0x");
